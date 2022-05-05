85fe5c84bb7f6e7f546a68b845303c402c4e520b
shadow3012 <pardeepitech@gmail.com>
2022-05-05 10:57:49 +0530

    recentView added

--- scripts/product_details.js
+++ scripts/product_details.js
@@ -6,8 +6,31 @@ document.getElementById("name").innerText = details.name
 document.getElementById("category").innerText = details.category
 document.getElementById("price").innerText = `US$${details.price},00`
 document.getElementById("price").style.textDecoration = "line-through"
-document.getElementById("off").innerText = `${details.off}%`
+document.getElementById("off").innerText = `${details.off}% OFF`
 document.getElementById("fPrice").innerText = `US$${details.finalPrice},00`
+if(details.color!=undefined){
+    document.getElementById("color").innerText=details.color
+    if(details.color=="Black"){
+        document.querySelector("#color_radio").style.accentColor = "black"
+    }
+    else if(details.color=="Black"){
+        document.querySelector("#color_radio").style.accentColor = "black"
+    }
+}
+else{
+    document.getElementById("color").innerText= "Black"
+    document.getElementById("color_radio").style.accentColor = "black"
+}
+let addToBag = document.createElement("button")
+addToBag.innerText = "ADD TO SHOPPING BAG"
+let addToDream = document.createElement("button")
+addToDream.innerText = "ADD TO DREAM BOX"
+let br = document.createElement("br")
+
+document.getElementById("topright").append(addToBag,br,addToDream)
+
+import { appendRecent } from "./moveToDiv.js"
+appendRecent("recentView")
 
 
 
