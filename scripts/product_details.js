c169b90519e3d5855071e75bfce2a9db4cf5580b
shadow3012 <pardeepitech@gmail.com>
2022-05-06 23:59:52 +0530

    add to button set to local storage

--- scripts/product_details.js
+++ scripts/product_details.js
@@ -1,4 +1,5 @@
 let details =JSON.parse(localStorage.getItem("detailed_product"))
+
 let mainImg = document.createElement("img")
 mainImg.src = details.img
 document.getElementById("topleft").append(mainImg)
@@ -21,9 +22,34 @@ else{
     document.getElementById("color").innerText= "Black"
     document.getElementById("color_radio").style.accentColor = "black"
 }
+
+let cartBag = JSON.parse(localStorage.getItem("addToCart"))||[]
 let addToBag = document.createElement("button")
+addToBag.addEventListener("click",()=>{
+    localStorage.setItem("addToCart",JSON.stringify(details))
+})
 addToBag.innerText = "ADD TO SHOPPING BAG"
 let addToDream = document.createElement("button")
+addToDream.addEventListener("click",()=>{
+    let value 
+    let dreamBox = JSON.parse(localStorage.getItem("dreamBox"))||[]
+    for(let x = 0;x<dreamBox.length;x++){
+        
+        let name = dreamBox[x].name
+        let category = dreamBox[x].category
+        let finalPrice = dreamBox[x].finalPrice
+        if(name == details.name && category==details.category&& details.finalPrice == finalPrice){
+            value = false
+        }
+    }
+    if(value!=false){
+        dreamBox.push(details)
+        localStorage.setItem("dreamBox",JSON.stringify(dreamBox))
+    }
+})
+function movetodream(details){
+    console.log(details)
+}
 addToDream.innerText = "ADD TO DREAM BOX"
 let br = document.createElement("br")
 
